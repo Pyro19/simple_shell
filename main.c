b27e618a03c6c6f54e8e4f2b7d3a7313415ce79f
enariyah <hopmichael52@gmail.com>
2022-09-07 22:47:46 +0300

    main fixed

--- main.c
+++ main.c
@@ -1,50 +1,5 @@
 #include "shell.h"
 /**
-* main - the main function
-* @ac: count of the argument
-* @av: argument vector
-* @env: enviroment variable
-* Return: 0, 1 on failure
-*/
-int main(int ac, char **av, char **env)
-{
-	char *buff = NULL, **cmd;
-	size_t len = 0;
-	ssize_t chars;
-	pid_t pid;
-	int status, count = 0;
-
-	cisfun();
-	while ((chars = getline(&buff, &len, stdin)))
-	{
-		signal(SIGINT, handler);
-		if (chars == EOF)
-			_eof(buff);
-		count++;
-		cmd = _strtok(buff);
-		pid = fork();
-		if (pid == -1)
-		void _perror(void);
-		if (pid == 0)
-			_execve(cmd, buff, env, av, count);
-		else
-		{
-			wait(&status);
-			if (cmd == NULL)
-			void free_if(void);
-			else if (_strcmp("exit", cmd[0]))
-			void free_elif(void);
-			else
-			void free_if(void);
-		}
-		len = 0, buff = NULL;
-		cisfun();
-	}
-	if (chars == -1)
-		return (EXIT_FAILURE);
-	return (EXIT_SUCCESS);
-}
-/**
  * _perror - prints error message
  * Return: Nothing
  */
@@ -73,22 +28,52 @@ void cisfun(void)
 		write(STDOUT_FILENO, "cisfun$ ", 8);
 }
 /**
- * free_if - function to free if
- * Return: Nothing
- */
-void free_if(void)
+* main - the main function
+* @ac: count of the argument
+* @av: argument vector
+* @env: enviroment variable
+* Return: 0, 1 on failure
+*/
+int main(int ac, char **av, char **env)
 {
-	free(buff);
-	free_all(cmd);
+	char *buff = NULL, **cmd;
+	size_t len = 0;
+	ssize_t chars;
+	pid_t pid;
+	int status, count = 0;
+	(void)ac;
+	cisfun();
+	while ((chars = getline(&buff, &len, stdin)))
+	{
+		signal(SIGINT, handler);
+		if (chars == EOF)
+			_eof(buff);
+		count++;
+		cmd = _strtok(buff);
+		pid = fork();
+		if (pid == -1)
+		_perror();
+		if (pid == 0)
+			_execve(cmd, buff, env, av, count);
+		else
+		{
+			wait(&status);
+			if (cmd == NULL)
+			free_if(buff, cmd);
+			else if (_strcmp("exit", cmd[0]))
+			free_elif(buff, cmd);
+			else
+			{
+				free(buff);
+				free_all(cmd);
+			}
+		}
+		len = 0, buff = NULL;
+		cisfun();
+	}
+	if (chars == -1)
+	{
+		return (EXIT_FAILURE);
+	}
+		return (EXIT_SUCCESS);
 }
-/**
- * free_elif - function frees the else if
- * Return: Nothing
- */
-void free_elif(void)
-{
-	free(buff);
-	free_all(cmd);
-	exit(EXIT_SUCCESS);
-}
-
