df5cc8e66332a04fa3a2b71c5d45c94032627049
Ashaolu Peter O <pyrotech.sv@gmail.com>
2022-09-07 05:28:32 -0700

    shell files headers

--- shell.h
+++ shell.h
@@ -1,76 +1,39 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
 #include <sys/types.h>
-
+#include <sys/wait.h>
 #include <sys/stat.h>
 
-#include <sys/wait.h>
-
-#include <stdlib.h>
-
-#include <string.h>
-
-#include <unistd.h>
-
-#include <signal.h>
-
-#include <stdio.h>
-
-
-
-extern char **environ;
-
-
-
-char *_strcat(char *dest, char *src);
-
-int _strlen(const char *s);
-
+void dec();
+void cisfun(void);
+void handler(int _signal);
+void _perror(void);
+void free_if(void);
+void free_elif(void);
+unsigned int len_cmd(char *str);
+char **_strtok(char *str);
+void _execve(char **cmd, char *buff, char **env, char **av, int count);
+char *_strcpy(char *s1, char *s2, int n);
+char *_strcpyconst(char *s1, const char *s2, int n);
+unsigned int _strconstlen(const char *s);
+unsigned int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
-
-int _strncmp(const char *s1, const char *s2, size_t len);
-
-char *_strdup(char *str);
-
-int _atoi(char *s);
-
-int _putchar(char c);
-
-void _puts(char *str);
-
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
-int count_input(char *str);
-
-int count_delims(char *str, char *del);
-
-char *remove_new_line(char *str);
-
-void signal_handler(int sig_id);
-
-void _open_help(void);
-
-void _print_env(void);
-
-void _handle_exit(char **u_tokns, char *line);
-
-int execBuiltInCommands(char **u_tokns, char *line);
-
-void frees_get_env(char *env_path);
-
-void frees_tokens(char **tokns);
-
-int exec(char *cname, char **opts);
-
-char *_getenv(const char *name);
-
-char **tokenize(char *str, char *del, int len);
-
-char *find(char *cname);
+unsigned int dir_num(char *path);
+char **c_d_pointer(char *fcmd, char **environ);
+char *_env(const char *name, char **environ);
+char *append_cmd(char *s1, char *s2, char *cmd, int n, int c);
+void print_env(char **environ);
+void free_all(char **ptr);
+void err_messenger(char **av, char *fcmd, int count);
+int _puterror(char c);
+void _eof(char *buff);
+void _path(char **cmd, char *buff, char **env, char **av, int count);
 
 
-
-#endif /* SHELL_H */
+#endif
